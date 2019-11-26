1fe4c839bdb0ff05e0e3929a14f99f6ff2430391
Drekavac <diesel39931@gmail.com>
2019-11-26 23:50:37 +0600

    fixed notice in browser console about firebase

--- src/base.js
+++ src/base.js
@@ -1,5 +1,7 @@
 import Rebase from 're-base';
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/storage';
 
 const firebaseApp = firebase.initializeApp({
     apiKey: "AIzaSyBUE37-LvHH5G_Db5mosTo5qLrqREYLq1I",
